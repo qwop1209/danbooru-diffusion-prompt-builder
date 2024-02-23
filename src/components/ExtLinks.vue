38be48bcd1deab98d41d9ebe0b00f84919249ceb
Yifei Liu <liu1456447215@gmail.com>
2024-02-24 03:23:27 +0800

    Update ExtLinks.vue

--- src/components/ExtLinks.vue
+++ src/components/ExtLinks.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: Yifei Liu liu1456447215@gmail.com
- * @Date: 2024-02-23 18:37:06
- * @LastEditors: Yifei Liu liu1456447215@gmail.com
- * @LastEditTime: 2024-02-23 18:53:38
- * @FilePath: /danbooru-diffusion-prompt-builder/src/components/ExtLinks.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <!------------------------------------------------------------------------------
   - Danbooru Diffusion Prompt Builder
   - Copyright (C) 2022  Jabasukuriputo Wang
